e11e9c01cf40bf96491f5e400ef70e75e86b5dd5
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-22 17:24:14 -0400

    commit tree c2811c6de2f315675c3817f4b5eb296929d647be

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const updateTitle = (title) => {
+  return title.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => updateTitle(title))
 }
